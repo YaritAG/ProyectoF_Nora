f55eb082a38280ae30552460201a8bbc44bdf185
Yarit Aguirre <yarit.aguirre6750@alumnos.udg.mx>
2024-11-13 00:39:33 -0600

    Terminar verlibros.php, y verUsuarios.php, empezar a registrar

--- public/admin/verLibros.php
+++ public/admin/verLibros.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -6,6 +10,54 @@
     <title>Admin | Ver Libros</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="seccion-tabla">
+
+            <!-- Seccion de Inputs -->
+            <div class="seccion1">
+                <form action="verLibros.php" method="POST">
+                    <div class="input-tabla">
+                        <label for="nombre">Nombre del libros</label><br>
+                        <input type="text" id="nombre" name="nombre" placeholder="Ingresa tu Nombre" required>
+                        <i class="icon user"></i>
+                    </div>
+                </form>
+            </div>
+
+            <!-- Seccion de la tabla -->
+            <div class="seccion2">
+                <h2>Tabla de Libros</h2>
+
+                <!-- Tabla de Libros CRUD de libros -->
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Ejemplar</th>
+                            <th>Editorial</th>
+                            <th>Género</th>
+                            <th>Páginas</th>
+                            <th>Año</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th>
+                                <button type="submit" class="btn-tabla">Borrar</button>
+                                <button type="sumbit" class="btn-tabla">Editar</button>
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
