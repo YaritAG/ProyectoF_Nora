c5574236bd546000741026ecdf34e3c32c438d16
Yarit Aguirre <yarit.aguirre6750@alumnos.udg.mx>
2024-11-14 22:34:22 -0600

    Cambuios

--- public/admin/ventanaAdmin.php
+++ public/admin/ventanaAdmin.php
@@ -1,6 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 
+// Verifica si el usuario está logueado y tiene rol de administrador
 if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'admin') {
     header('Location: ../../templates/menu.php'); // Redirige si no es administrador
     exit;
@@ -13,35 +17,33 @@ echo "Bienvenido al panel de administración";
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <link rel="stylesheet" href="static/admin.css">
 
     <!-- Quicksand -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&display=swap" 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&family=Quicksand:wght@300..700&display=swap"
         rel="stylesheet">
 </head>
+
 <body>
-    <nav class="barra-superior">
-        <div class="botones-navbar">
-            <a href="verLibros.php" class="btn-verLibros">Ver Libros</a>
-            <a href="verPrestamos.php" class="btn-verPrestamos">Ver Prestamos</a>
-            <a href="../../templates/menu.php" class="btn-menu">Menú Principal</a>
-            <a href="ventanaAdmin.php" class="btn-admin">Menu de Admin</a>
-            <a href="verUsuarios.php" class="btn-users">Ver Usuarios</a>
-        </div>
-        
-        <div class="logout">
-            <a href="../public/user/perfil.php" class="btn-perfil">Mi Perfil</a>
-            <a href="../public/user/login.php" class="btn-logout">Cerrar Sesión</a>
-        </div>
+    <nav class="barra-superior" style="background-color: #333; color: #fff; padding: 10px;">
+    <!-- contenido de la barra de navegación -->
     </nav>
+    <!-- Incluye la barra de navegación desde el archivo headerAdmin.php -->
+    <?php include '../../assets/partials/headerAdmin.php'; ?>
 
     <div class="container">
         <h1>Bienvenido admin</h1>
+        <!-- Aquí puedes agregar más contenido exclusivo para el administrador -->
     </div>
+
 </body>
+
 </html>
